291c84dac65fcb65303816aad806f09f5b4a420f
kpwwalsh <wnuru@hotmail.com>
2019-08-11 20:17:01 -0500

    assignment prompt and menu

---
+++ controllers/MenuController.js
@@ -0,0 +1,61 @@
+const inquirer = require('inquirer');
+
+ module.exports = class MenuController {
+   constructor(){
+    this.mainMenuQuestions = [
+        {   
+         type: "list",
+          name: "mainMenuChoice",
+          message: "Please choose from an option below: ",
+          choices: [
+            "Add new contact",
+            "Date and Time",
+            "Exit" 
+          ]
+        }
+      ];
+    this.contacts = [];
+   }
+
+ main(){
+    console.log(`Welcome to AddressBloc!`);
+    inquirer.prompt(this.mainMenuQuestions).then((response) => {
+      switch(response.mainMenuChoice){
+        case "Check date and time":
+           this.getDate();
+           break;
+        case "Add new contact":
+          this.addContact();
+          break;
+        case "Exit":
+          this.exit();
+        default:
+          console.log("Invalid input");
+          this.main();
+      }
+    })
+      .catch((err) => {
+       console.log(err);
+    });
+  }
+
+  getDate(){
+    this.clear();
+    var date= Date();
+    console.log(date)
+    this.main();
+      }
+
+  clear(){/*method definition*/}
+    addContact(){
+        this.clear();
+        console.log('addContact called');
+        this.main();
+      }
+   
+  exit(){
+        console.log("Thanks for using AddressBloc!");
+        process.exit();
+      }
+   }
+ 
