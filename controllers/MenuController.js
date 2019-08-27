bd88985d94eba4344f1b23fd4c574d6fe463bd7f
kpwwalsh <wnuru@hotmail.com>
2019-08-27 18:13:17 -0500

    debug dependencies creating-contacts

---
+++ controllers/MenuController.js
@@ -0,0 +1,76 @@
+const inquirer = require('inquirer');
+const ContactController = require("./ContactController");
+
+ module.exports = class MenuController {
+   constructor(){
+    this.mainMenuQuestions = [
+        {   
+         type: "list",
+          name: "mainMenuChoice",
+          message: "Please choose from an option below: ",
+          choices: [
+            "Add new contact",
+            "Date and Time",
+            "Exit" 
+          ]
+        }
+      ];
+      this.book = new ContactController();
+   }
+
+ main(){
+    console.log(`Welcome to AddressBloc!`);
+    inquirer.prompt(this.mainMenuQuestions).then((response) => {
+      switch(response.mainMenuChoice){
+        case "Check date and time":
+           this.getDate();
+           break;
+        case "Add new contact":
+          this.addContact();
+          break;
+        case "Exit":
+          this.exit();
+        default:
+          console.log("Invalid input");
+          this.main();
+      }
+    })
+      .catch((err) => {
+       console.log(err);
+    });
+  }
+
+  getDate(){
+    this.clear();
+    var date= Date();
+    console.log(date)
+    this.main();
+      }
+
+  clear(){/*method definition*/}
+    addContact(){
+        this.clear();
+        inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+          this.book.addContact(answers.name, answers.phone).then((contact) => {
+            console.log("Contact added successfully!");
+            this.main();
+          }).catch((err) => {
+            console.log(err);
+            this.main();
+          });
+        });
+      }
+   
+  exit(){
+        console.log("Thanks for using AddressBloc!");
+        process.exit();
+      }
+ 
+   getContactCount(){
+    return this.contacts.length;
+  }
+//assignment function
+  remindMe(){
+    return "learning is a life-long pursuit";
+   }
+  };
---
+++ db/migrations/20190827180831-add-email-to-contacts.js
@@ -0,0 +1,14 @@
+"use strict";
+module.exports={
+up: (queryInterface, Sequelize) => {
+  return queryInterface.addColumn("Contacts", "email", {                   
+      allowNull: false,      
+      type: Sequelize.STRING 
+  });
+},
+
+down: function(queryInterface, Sequelize) {
+  // logic for reverting the changes
+  return queryInterface.removeColumn("Contacts", "email");
+ }
+}
